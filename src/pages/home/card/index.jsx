4ceb179ebe2dda7a2ceae96103775ecc0784ca0b
DavletovDilmurad <you@example.com>
2024-05-08 16:39:28 +0500

    first commit

--- src/pages/home/card/index.jsx
+++ src/pages/home/card/index.jsx
@@ -1,45 +1,77 @@
-import React from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import '../../../style/main.css';
+import { Navigation } from 'swiper/modules';
 
 function Card() {
+  const list = [
+    {
+      id: 1,
+      title: 'Играй уже сейчас в Minecraft',
+      price: 4999,
+      discount: 25,
+      oldprice: 6999,
+      image: '/card/card-img1.png',
+    },
+    {
+      id: 2,
+      title: 'Играй уже сейчас в Minecraft dangerous',
+      price: 4999,
+      discount: 25,
+      oldprice: 6999,
+      image: '/card/card-img2.png',
+    },
+    {
+      id: 3,
+      title: 'Играй уже сейчас в Minecraft dangerous',
+      price: 6999,
+      discount: 15,
+      oldprice: 7999,
+      image: '/card/card-img3.png',
+    },
+    {
+      id: 4,
+      title: 'Играй уже сейчас в Minecraft dangerous',
+      price: 4999,
+      discount: 25,
+      oldprice: 6999,
+      image: '/card/card-img4.png',
+    },
+  ]
+
+  const width = window.screen.width
+
   return (
     <section className='mt-14 mb-20'>
       <div className="container">
-        <div className="flex items-center gap-5">
-          <div className='w-1/2 h-96 rounded-xl bg-gradient-to-br from-lime-400 p-10 relative flex flex-col justify-between'>
-            <div className='relative z-10'>
-              <p className='font-extrabold text-3xl w-96 mb-5'>Играй уже сейчас в Minecraft</p>
-              <div className='flex items-end gap-5'>
-                <p className='font-extrabold text-2xl text-white'>4 999 Р</p>
-                <p className='font-extrabold text-lg text-yellow-300'>-25%</p>
-                <p className='font-extrabold text-lg text-neutral-600 line-through'>6 999 Р</p>
-              </div>
-            </div>
-            <div className='flex items-center gap-2'>
-              <button className='mt-5 py-3 px-10 relative z-40 bg-transparent font-extrabold text-lg text-white border-transparent border-solid border-slate-500 hover:bg-white hover:border-white hover:text-black rounded-lg ease-in duration-300'>Купить</button>
-              <button className='mt-5 py-3 px-10 relative z-40 border-solid border-white bg-white font-extrabold text-lg text-black rounded-lg hover:bg-transparent hover:border-solid hover:text-white ease-in duration-300'>В избранное</button>
-            </div>
-            <div className='absolute z-0 right-4' style={{ top: '-54px' }}>
-              <img src="/card/card-img1.png" alt="card image" />
-            </div>
+        <Swiper slidesPerView={width >= 770 ? 2 : 1} navigation={true} modules={[Navigation]} className="mySwiper ">
+          <div className="flex items-center gap-5">
+            {
+              list.map(item => (
+                <SwiperSlide className='w-1/2 h-96 rounded-xl bg-gradient-to-br from-lime-400 p-10 relative flex flex-col justify-between mr-3' key={item.id}>
+                  <div>
+                    <div className='relative z-10'>
+                      <p className='font-extrabold text-3xl w-96 mb-5 max-[970px]:text-xl max-[830px]:ml-0 max-[830px]:text-base max-[830px]:w-auto'>{item.title}</p>
+                      <div className='flex items-end gap-5'>
+                        <p className='font-extrabold text-2xl text-white max-[970px]:text-base'>{item.price} Р</p>
+                        <p className='font-extrabold text-lg text-yellow-300 max-[970px]:text-base'>-{item.discount}%</p>
+                        <p className='font-extrabold text-lg text-neutral-600 line-through max-[970px]:text-base'>{item.oldprice} Р</p>
+                      </div>
+                    </div>
+                    <div className='flex items-center gap-2'>
+                      <button className='mt-5 py-3 px-10 relative z-40 bg-transparent font-extrabold text-lg text-white border-transparent border-solid border-slate-500 hover:bg-white hover:border-white hover:text-black rounded-lg ease-in duration-300 max-[970px]:px-5 max-[970px]:py-2 max-[970px]:text-base'>Купить</button>
+                      <button className='mt-5 py-3 px-10 relative z-40 border-solid border-white bg-white font-extrabold text-lg text-black rounded-lg hover:bg-transparent hover:border-solid hover:text-white ease-in duration-300 max-[970px]:px-5 max-[970px]:py-2 max-[970px]:text-base'>В избранное</button>
+                    </div>
+                    <div className='absolute z-0 right-4 top-10 w-[250px] h-[300px]'>
+                      <img src={item.image} alt="card image" className='w-full h-full object-contain' />
+                    </div>
+                  </div>
+                </SwiperSlide>
+              ))
+            }
           </div>
-          <div className='w-1/2 h-96 rounded-xl bg-gradient-to-b from-yellow-400 p-10 relative flex flex-col justify-between'>
-            <div className='relative z-10'>
-              <p className='font-extrabold text-3xl w-96 mb-5'>Играй уже сейчас в Minecraft dangerous</p>
-              <div className='flex items-end gap-5'>
-                <p className='font-extrabold text-2xl text-white'>4 999 Р</p>
-                <p className='font-extrabold text-lg text-yellow-300'>-25%</p>
-                <p className='font-extrabold text-lg text-neutral-600 line-through'>6 999 Р</p>
-              </div>
-            </div>
-            <div className='flex items-center gap-2'>
-              <button className='mt-5 py-3 px-10 relative z-40 bg-transparent font-extrabold text-lg text-white border-transparent border-solid border-slate-500 hover:bg-white hover:border-white hover:text-black rounded-lg ease-in duration-300'>Купить</button>
-              <button className='mt-5 py-3 px-10 relative z-40 border-solid border-white bg-white font-extrabold text-lg text-black rounded-lg hover:bg-transparent hover:border-solid hover:text-white ease-in duration-300'>В избранное</button>
-            </div>
-            <div className='absolute z-0' style={{ top: '-100px', right: '-100px'}}>
-              <img src="/card/card-img2.png" alt="card image" />
-            </div>
-          </div>
-        </div>
+        </Swiper>
       </div>
     </section>
   )
--- src/pages/home/recommended-list/index.jsx
+++ src/pages/home/recommended-list/index.jsx
@@ -1,5 +1,5 @@
 
-import { FavouritesIcon } from '../../../assets/icons'
+import { CartIcon, FavouritesIcon } from '../../../assets/icons'
 function RecommendedList() {
 
     const list = [
@@ -48,25 +48,30 @@ function RecommendedList() {
                     <div className="flex gap-5 max-[1060px]:gap-3 max-lg:flex-wrap max-[500px]:gap-5 max-[574px]:flex-col">
                         {
                             list.map(item => (
-                                <div className='w-80 h-3/4 relative max-[1040px]:w-[235px] max-[1006px]:w-[280px] max-[955px]:w-[260px] max-[840px]:w-[240px] max-[774px]:w-[220px] max-[716px]:w-[200px] max-[653px]:w-[300px] max-[642px]:w-[280px] max-[604px]:w-[260px] max-[574px]:flex max-[574px]:w-full' key={item.id}>
-                                    <div className='w-full h-full cursor-pointer'>
-                                        <img src={item.image} alt={item.title} className='w-full h-full object-cover' />
+                                <div className='w-80 h-3/4 relative max-[1040px]:w-[235px] max-[1006px]:w-[280px] max-[955px]:w-[260px] max-[840px]:w-[240px] max-[774px]:w-[220px] max-[716px]:w-[200px] max-[654px]:w-[300px] max-[642px]:w-[280px] max-[604px]:w-[260px] max-[574px]:flex max-[574px]:w-full' key={item.id}>
+                                    <div className='w-full h-full cursor-pointer max-[574px]:w-[300px] max-[574px]:mr-3 max-[574px]:h-[400px] max-[574px]:rounded-2xl overflow-hidden max-[500px]:h-[340px] max-[450px]:h-[250px] max-[450px]:w-[200px] max-[390px]:w-[150px] max-[390px]:h-[200px] max-[310px]:h-[180px]'>
+                                        <img src={item.image} alt={item.title} className='w-full h-full' />
                                     </div>
-                                    <div className='p-4'>
-                                        <div className='flex items-center gap-5 mb-2'>
-                                            <p className="text-white font-medium text-2xl">{item.price.toLocaleString()} P</p>
-                                            <p className="font-medium text-lg text-lime-500">-{item.discount}%</p>
-                                            <p className="font-normal text-lg text-neutral-700 line-through">{item.oldprice.toLocaleString()} P</p>
+                                    <div className='p-4 max-[684px]:p-2 max-[574px]:p-1 max-[435px]:p-0'>
+                                        <div className='flex items-center gap-5 mb-2 max-[1154px]:gap-4 max-md:gap-3 max-[684px]:gap-2 max-[574px]:gap-4 max-[365px]:gap-1 max-[435px]:gap-1'>
+                                            <p className="text-white font-medium text-2xl max-[1218px]:text-xl max-lg:text-base max-[684px]:text-sm max-[574px]:text-base max-[435px]:text-[13px] max-[365px]:text-[12px] max-[300px]:text-[10px]">{item.price.toLocaleString()} P</p>
+                                            <p className="font-medium text-lg text-lime-500 max-lg:text-base max-[684px]:text-sm max-[574px]:text-sm max-[435px]:text-[13px] max-[365px]:text-[10px] max-[300px]:text-[9px]">-{item.discount}%</p>
+                                            <p className="font-normal text-lg text-neutral-700 line-through max-lg:text-base max-[684px]:text-sm max-[574px]:text-xs max-[435px]:text-[13px] max-[365px]:text-[10px] max-[300px]:text-[9px]">{item.oldprice.toLocaleString()} P</p>
                                         </div>
-                                        <p className="font-normal text-base text-white mb-2 h-12">{item.title}</p>
-                                        <button className='hidden bg-white rounded-full p-2.5 absolute top-5 right-8 hover:block'>
+                                        <div>
+                                            <p className="font-normal text-base text-white mb-2 h-12 max-[574px]:w-2/4 max-[574px]:h-full max-md:text-sm max-[574px]:text-xs max-[465px]:text-[12px] max-[465px]:w-full">{item.title.split(' ').slice(0, 3).join(' ')}</p>
+                                        </div>
+                                        <button className='bg-white rounded-full p-2.5 absolute top-5 right-3 max-[574px]:bg-transparent max-[574px]:top-[90%] max-[574px]:right-10'>
                                             <FavouritesIcon />
                                         </button>
-                                        <button className='absolute bottom-1/4 h-14 border-solid border-transparent bg-lime-500 w-64 rounded-xl p-3 mb-5 ease-in-out duration-300  hover:bg-transparent hover:border-solid hover:border-white font-bold text-lg'>
+                                        <button className='bg-white rounded-full p-2.5 absolute top-5 right-3 max-[574px]:bg-transparent max-[574px]:top-[90%] max-[574px]:right-0'>
+                                            <CartIcon />
+                                        </button>
+                                        <button className='absolute bottom-1/4 max-[1219px]:bottom-[170px] max-[1200px]:w-10/12 max-md:text-base max-md:h-10 max-md:p-1 max-[684px]:bottom-32 max-[670px]:bottom-36 max-[654px]:w-[260px] max-[642px]:bottom-44 max-[642px]:w-[250px max-[604px]:w-[220px] max-[574px]:hidden h-14 border-solid border-transparent bg-lime-500 w-64 rounded-xl p-3 mb-5 ease-in-out duration-300  hover:bg-transparent hover:border-solid hover:border-white font-bold text-lg'>
                                             В корзину
                                         </button>
-                                        <span className='absolute top-3 max-[574px]:right-0 max-[574px]:bottom-0 max-[574px]:h-8'>
-                                            <img src="/recommendedList/listtoptitle.svg" alt="list top title svg" />
+                                        <span className='absolute top-3 max-[574px]:h-8 max-[574px]:right-32 max-[546px]:right-20 max-[574px]:top-[90%] max-[450px]:right-28 max-[420px]:right-20 max-[450px]:h-6 max-[390px]:top-[86%] max-[390px]:right-32 max-[360px]:right-20 max-[312px]:right-14'>
+                                            <img src="/recommendedList/listtoptitle.svg" alt="list top title svg" className='w-full h-full' />
                                         </span>
                                     </div>
                                 </div>
